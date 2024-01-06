ae45dbc23b7b7f6ba987006567e3c6c5e8f9d069
Vlad <vladut_panaite@yahoo.com>
2024-01-06 17:14:57 +0200

    v.0.2

--- AiCarDetection/Entities/Car.py
+++ AiCarDetection/Entities/Car.py
@@ -1,9 +1,50 @@
 
 
 class Car():
+    
+    MAX_TIME_STATIONARY = 1
+    STATIONARY_THRESHOLD = 20
+    
     def __init__(self):
         self.car_id = None
-        self.car_status = None #parked or not
+        self.car_parked = None #parked or not
+        self.car_previous_position = None
         self.car_position = None
-        self.count_status = None
+        self.time_stationary_start = None
+        self.time_stationary_end = None
+        self.time_stationary = None
+        self.time_elapsed_feed = None
+    
+    def checkTime(self):
+        if self.car_position is not None and self.car_previous_position is not None:
+            current_pos = self.car_position
+            previous_pos = self.car_previous_position
+            motion_speed = abs(current_pos[0] - previous_pos[0]) + abs(current_pos[1] - previous_pos[1])
+            # update stationary time if counters are set
+            if self.time_stationary_start and self.time_stationary_end:
+                self.time_stationary = self.time_stationary_end - self.time_stationary_start
+                self.car_parked = self.time_stationary > self.MAX_TIME_STATIONARY
+            # if is stationary , set start_counter
+            if motion_speed < self.STATIONARY_THRESHOLD:
+                if self.time_stationary_start is None:
+                    self.time_stationary_start = self.time_elapsed_feed
+                else:
+                    self.time_stationary_end = self.time_elapsed_feed
+            else:
+            # if is moving, reset counters          
+                if self.time_stationary_start is not None:
+                    self.time_stationary_end = None
+                    self.time_stationary_start = None  
+                      
+        return
         
+    def updateCar(self, car_id, car_previous_position, car_position, time_elapsed):    
+        self.car_id = car_id
+        self.car_position = car_position
+        self.car_previous_position = car_previous_position
+        self.time_elapsed_feed = time_elapsed
+        self.car_parked = False
+        self.checkTime()
+       
+    def __str__ (self):
+        return "car_id={0} ; car_parked={1} ; car_position:{2};".format(self.car_id, self.car_parked, self.car_position)
--- AiCarDetection/ai_controller.py
+++ AiCarDetection/ai_controller.py
@@ -12,6 +12,7 @@ previous_bbox = None
 stationary_threshold = 20
 frame_count = 20
 parked = []
+cars = {}
 def get_bbox_from_list(list_bbox, tracker_id):
     for bbox in list_bbox:
         if bbox[4] != None and bbox[4] == tracker_id:
@@ -35,39 +36,60 @@ def is_stationary(previous_bbox, current_bbox, threshold):
             parked.append(False)
     return parked
 
-def process_frame_tracking(frame: np.ndarray) -> np.ndarray:
+def update_cars(detections :sv.Detections, prev_detection : sv.Detections, time_elapsed):
+    global cars
+    
+    for item in prev_detection:
+        car = None
+        tracker_id = item[4]
+        if len(cars) > 0 and tracker_id in cars:
+            car = cars[item[4]]
+        else:
+            car = Car()
+        previous_pos = item[0]
+        curr_i = get_bbox_from_list(detections, tracker_id)
+        if curr_i:
+            current_pos = curr_i[0]
+            car.updateCar(tracker_id,previous_pos, current_pos, time_elapsed)
+        cars[car.car_id] = car 
+
+
+def process_frame_tracking(frame: np.ndarray, time_elapsed) -> np.ndarray:
     # detect
     global previous_bbox
     global frame_count
     global parked
-    results = model(frame)[0]
+    results = model(frame,verbose=False)[0]
     detections = sv.Detections.from_ultralytics(results)
-    detections = detections[detections.class_id == 2]
+    #detections = detections[detections.class_id == 2]
     detections = tracker.update_with_detections(detections)
-    #print(frame_count)
     frame_count = frame_count - 1
     current_parked = parked
+    detection_zone_mask = zone.trigger(detections=detections)
+    detection_zone_in = detections[detection_zone_mask]
+    detections = detection_zone_in
     current_bbox = detections
+    
     if len(current_parked) < len(detections):
         current_parked.extend([False] * (len(detections) - len(current_parked)))  # Extend list1 with zeros
-    if frame_count != 0:
-        print("")  
-    else:
+    if frame_count == 0:
+        print(" --Reset Frame Counter-- ")  
         if previous_bbox :
-            print("Start checking")
             parked = is_stationary(previous_bbox, current_bbox, stationary_threshold)
+            update_cars(current_bbox, previous_bbox, time_elapsed)
         frame_count = 20
+        for car in cars: print(cars[car])
         previous_bbox = current_bbox
-    zone.trigger(detections=detections)
+
     labels = []
     for class_id, tracker_id, parked_value in zip(detections.class_id, detections.tracker_id, current_parked):
         labels.append(f"#{tracker_id} {results.names[class_id]} #parked={parked_value}")
               
-  
     annotated_frame = box_annotator.annotate(frame.copy(), detections=detections)
     annotated_frame = label_annotator.annotate(annotated_frame, detections=detections, labels=labels)
     annotated_frame = trace_annotator.annotate(annotated_frame, detections=detections)
     annotated_frame = zone_annotator.annotate(scene=annotated_frame)
+    
     return annotated_frame
 
 
@@ -78,10 +100,16 @@ polygon = np.array([
     [315, 205],
     [340, 90],
 ])
+polygon = np.array([
+    [522 , 202],
+    [530, 703],
+    [830, 595],
+    [675, 180],
+])
 video_path = "AiCarDetection/input/video.mp4"
 video_info = sv.VideoInfo.from_video_path(video_path)
 zone = sv.PolygonZone(polygon=polygon, frame_resolution_wh=video_info.resolution_wh)
-model = YOLO('yolov8s.pt')
+model = YOLO('yolov8n.pt')
 # initiate annotators
 zone_annotator = sv.PolygonZoneAnnotator(zone=zone, color=sv.Color.white())
 tracker = sv.ByteTrack()
@@ -90,12 +118,16 @@ label_annotator = sv.LabelAnnotator()
 trace_annotator = sv.TraceAnnotator()
 cap = cv2.VideoCapture(video_path)
 # Loop through the video frames
+width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+print('width:  ', width)
+print('height: ', height)
 while cap.isOpened():
     # Read a frame from the video
     success, frame = cap.read()
-
+   # print(str(cap.get(cv2.CAP_PROP_POS_MSEC)/1000) + "\r")
     if success:
-        annotated_frame = process_frame_tracking(frame=frame)
+        annotated_frame = process_frame_tracking(frame=frame,time_elapsed=(cap.get(cv2.CAP_PROP_POS_MSEC)/1000))
         # Display the annotated frame
         cv2.imshow("YOLOv8 Tracking", annotated_frame)
         if dete:
