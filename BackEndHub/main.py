486f534023076db40e459ac7763888b8ba3ebef1
Adrien COSTANDI <adrien.costandi@gmail.com>
2024-02-03 22:04:42 +0200

    frontend displays infractions

--- BackEndHub/main.py
+++ BackEndHub/main.py
@@ -1,38 +1,15 @@
 from typing import Union
 import uvicorn
 from fastapi import FastAPI
-from fastapi import Request, Depends,HTTPException
-from sse_starlette.sse import EventSourceResponse
+from fastapi import Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
 import logging
-import sys
 import asyncio
 import json
 from random import randint
 
-class SSEManager:
-    def __init__(self):
-        self.subscriptions = []
-
-    def add_subscription(self, response):
-        self.subscriptions.append(response)
-
-    def remove_subscription(self, response):
-        self.subscriptions.remove(response)
-
-    async def broadcast_message(self, message: dict):
-        for response in self.subscriptions:
-            await response.send_text(f"data: {json.dumps(message)}\n\n")
-
-sse_manager = SSEManager()
 app = FastAPI()
-
-STREAM_DELAY = 10  # second
-RETRY_TIMEOUT = 15000  # milisecond
-cnt = 0
-
-data_to_be_sent = None
 
 app.add_middleware(
     CORSMiddleware,
@@ -42,19 +19,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+fake_coordinates = [[38.248736, 21.738931], [38.246637, 21.736243], [38.247328, 21.737185]]
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.info('API is starting up')
 
+async def get_body(request: Request):
+    return await request.body()
+
 # Function to generate fake data
 async def generate_fake_data():
     while True:
-        # Generate fake data (replace this with your actual data generation logic)
-        value={"id": "ID_"+str(randint(1000000000,9999999999)), "type": "TrafficViolation", "location": {"value": {"type": "Point", "coordinates": [100, 100 ]}, "type": "GeoProperty"}, "description": {"type": "String", "value": "Illegal parking"}, "seeAlso": {"value": [], "type": "array"}}
-
-        fake_item = {"data":[value]}
-        yield f"data: {json.dumps(fake_item)}\n\n"
-        await asyncio.sleep(randint(1, 10))  # Sleep for 1 second
+        num_items = randint(1, 3)  # Generate a random number of items
+        values = []
+        for _ in range(num_items):
+            value={"id": "ID_"+str(randint(1000000000,9999999999)), "type": "TrafficViolation", "location": {"value": {"type": "Point", "coordinates": fake_coordinates[randint(0, len(fake_coordinates)-1)]}, "type": "GeoProperty"}, "description": {"type": "String", "value": "Illegal parking"}, "seeAlso": {"value": [], "type": "array"}}
+            values.append(value)
+        yield f"data: {json.dumps(values)}\n\n"
+        await asyncio.sleep(randint(1, 5))  # Sleep for 1 second
 
 # Define an endpoint to stream fake data
 @app.get("/sseFake")
@@ -66,42 +49,6 @@ async def stream_fake_data(request: Request):
     return StreamingResponse(fake_data_generator(), media_type="text/event-stream")
 
 
-async def get_body(request: Request):
-    return await request.body()
-
-
-@app.post("/notify")
-def getDataFromContextBroker(body: bytes = Depends(get_body)):
-   global data_to_be_sent
-   data_to_be_sent = body.decode('utf8')
-   logger.info(data_to_be_sent)
-   return body
-
-
-
-async def sse_generator():
-    global data_to_be_sent, cnt
-    while True:
-        if data_to_be_sent is not None:
-            cnt = cnt + 1
-            message = {'id': cnt,
-                    'data': 'data_body'}
-            message['data'] = data_to_be_sent
-            data_to_be_sent = None
-            yield f"data: {json.dumps(message)}\n\n"
-        await asyncio.sleep(1)
-    # Adjust the sleep interval as needed
-
-@app.get("/sse", response_class=StreamingResponse)
-async def sse_endpoint(request: Request):
-    response = StreamingResponse(sse_generator(), media_type="text/event-stream")
-    sse_manager.add_subscription(response)
-    logger.info(len(sse_manager.subscriptions))
-    return response
-
-
-
-
 # when you want to run in on the host
-if __name__ == "__main__": 
+if __name__ == "__main__":
    uvicorn.run("main:app", port=5000, log_level="info")
--- FrontEndApp/src/routes/map/Map.svelte
+++ FrontEndApp/src/routes/map/Map.svelte
@@ -1,31 +1,68 @@
 <script>
-  import { onMount } from 'svelte';
-  import 'leaflet/dist/leaflet.css';
+    import { onMount } from "svelte";
+    import "leaflet/dist/leaflet.css";
 
-  export let xCoord;
-  export let yCoord;
-  let map;
+    /**
+     * @type {number}
+     */
+    export let latitude;
+    /**
+     * @type {number}
+     */
+    export let longitude;
+    
+    /**
+     * @type {import("svelte/store").Writable<object>}
+     */
+    export let infractionsStore; // Declare a prop for the infractions store
 
-  onMount(() => {
-    if (typeof window !== 'undefined') {
-      import('leaflet').then(L => {
-        map = L.map('map').setView([xCoord, yCoord], 16); // Athens coordinates
-    
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          attribution: '&copy; OpenStreetMap contributors'
-        }).addTo(map);
-    
-        L.marker([xCoord, yCoord]).addTo(map)
-          .bindPopup('infraction');
-      });
-    }
-  });
+    let map;
+
+    onMount(() => {
+        if (typeof window !== "undefined") {
+            import("leaflet").then((L) => {
+                map = L.map("map").setView([latitude, longitude], 16);
+
+                L.tileLayer(
+                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    {
+                        attribution: "&copy; OpenStreetMap contributors",
+                    },
+                ).addTo(map);
+
+                // Subscribe to changes in the infractions store
+                const unsubscribe = infractionsStore.subscribe(
+                    (infractions) => {
+                        // Clear existing markers
+                        map.eachLayer((layer) => {
+                            if (layer instanceof L.Marker) {
+                                map.removeLayer(layer);
+                            }
+                        });
+
+                        // Add markers for each infraction
+                        infractions.forEach((infraction) => {
+                            L.marker([
+                                infraction.location.value.coordinates[0],
+                                infraction.location.value.coordinates[1],
+                            ])
+                                .addTo(map)
+                                .bindPopup("infraction.description");
+                        });
+                    },
+                );
+
+                // Unsubscribe from the store when component is destroyed
+                return unsubscribe;
+            });
+        }
+    });
 </script>
 
-<style>
-  #map {
-    height: 90vh;
-  }
-</style>
-
 <div id="map"></div>
+
+<style>
+    #map {
+        height: 100vh;
+    }
+</style>
